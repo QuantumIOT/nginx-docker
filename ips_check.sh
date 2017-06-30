09397faf0ce46d734270343a0f679116a66d1f1f
gprivitera <gpriviter4@gmail.com>
2017-06-30 15:00:21 -0400

    single line string for all ips

--- ips_check.sh
+++ ips_check.sh
@@ -9,14 +9,14 @@ if [ $status -ne 0 ]; then
 fi
 
 # catch current IPs
-current_ips=$(curl --unix-socket /var/run/docker.sock http:/v1.30/tasks | jq -c '.[] | {addresses: .NetworksAttachments[].Addresses[], id: .ID}');
+current_ips=$(curl --unix-socket /var/run/docker.sock http:/v1.30/tasks | jq -r -j '.[] | {addresses: .NetworksAttachments[].Addresses[], id: .ID}');
 
 # lets not relad in the first 30 seconds
 sleep 30;
 
 while /bin/true; do
 
-  new_ips=$(curl --unix-socket /var/run/docker.sock http:/v1.30/tasks | jq -c '.[] | {addresses: .NetworksAttachments[].Addresses[], id: .ID}')
+  new_ips=$(curl --unix-socket /var/run/docker.sock http:/v1.30/tasks | jq -r -j '.[] | {addresses: .NetworksAttachments[].Addresses[], id: .ID}');
 
   if [ "$new_ips" == "$current_ips" ]; then
     echo "It looks like the IPs have changed, restarting nginx";
