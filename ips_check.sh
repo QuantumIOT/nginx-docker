ddb50d0b5db031378293d010635866a9e23f9c20
gprivitera <gpriviter4@gmail.com>
2017-07-03 10:49:54 -0400

    silence curl debugging messages

--- ips_check.sh
+++ ips_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl --unix-socket /var/run/docker.sock http:/v1.30/tasks >/dev/null 2>&1
+curl -s --unix-socket /var/run/docker.sock http:/v1.30/tasks >/dev/null 2>&1
 
 status=$?
 if [ $status -ne 0 ]; then
@@ -9,19 +9,19 @@ if [ $status -ne 0 ]; then
 fi
 
 # catch current IPs
-current_ips=$(curl --unix-socket /var/run/docker.sock http:/v1.30/tasks | jq -r -j ".[] | .NetworksAttachments[].Addresses[] + .ID");
+current_ips=$(curl -s --unix-socket /var/run/docker.sock http:/v1.30/tasks | jq -r -j ".[] | .NetworksAttachments[].Addresses[] + .ID");
 
 # lets not relad in the first 30 seconds
 sleep 30;
 
 while /bin/true; do
 
-  new_ips=$(curl --unix-socket /var/run/docker.sock http:/v1.30/tasks | jq -r -j ".[] | .NetworksAttachments[].Addresses[] + .ID");
+  new_ips=$(curl -s --unix-socket /var/run/docker.sock http:/v1.30/tasks | jq -r -j ".[] | .NetworksAttachments[].Addresses[] + .ID");
 
   if [ "$new_ips" != "$current_ips" ]; then
     echo "It looks like the IPs have changed, restarting Nginx";
 
-    if [ -z "$SLACK_HOOK_URL" ]; then
+    if [ -n "$SLACK_HOOK_URL" ]; then
       curl -X POST --data-urlencode payload="{\"text\": \"Nginx container $(hostname) reloaded in $ENV environment because it detected IPs changes\"}" $SLACK_HOOK_URL;
     fi
 
